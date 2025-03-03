5a2ffd92715d68a6b3630504696d1a443ff0eee2
Ayush Jain <ayushjain2205@gmail.com>
2025-03-03 17:15:21 +0530

    Fix build eror

--- lib/client.ts
+++ lib/client.ts
@@ -2,5 +2,5 @@ import { createThirdwebClient } from "thirdweb";
 
 // Initialize the client
 export const client = createThirdwebClient({
-  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!,
+  clientId: process.env.NEXT_PUBLIC_THIRDWEB_PROJECT_ID!,
 });
